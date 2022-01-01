c73998de3001583f935cf591b514610ce20355dd
RYOTA-KOBA <ryotakobayashi04@gmail.com>
2022-01-01 16:14:00 +0900

    fix: rubocop 修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,5 @@
 class Attendance < ApplicationRecord
-  has_many :rest
+  has_many :rest, dependent: :destroy
   belongs_to :employee
   belongs_to :clock_in, optional: true
   belongs_to :clock_out, optional: true
